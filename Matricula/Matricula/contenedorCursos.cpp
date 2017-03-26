d7697834305c2056a4fe3da24a1deb6579dbd4fb
Steven Cedeño <stevenced@gmail.com>
2017-03-25 22:27:48 -0600

    Agrego contenedorCursos

---
+++ Matricula/Matricula/contenedorCursos.cpp
@@ -0,0 +1,46 @@
+#include "contenedorCursos.h"
+
+Contenedor_Cursos::Contenedor_Cursos()
+{
+	pinicio = NULL;
+	paux = NULL;
+}
+
+void Contenedor_Cursos::insertaInicio(Curso *unCurso)
+{
+	paux = new Nodo_Cursos(unCurso, NULL);
+
+	if (pinicio == NULL)
+	{
+		pinicio = paux;
+	}
+	else
+	{
+		paux->setNext(pinicio);
+		pinicio = paux;
+	}
+}
+
+string Contenedor_Cursos::toString()
+{
+	stringstream s;
+	paux = pinicio;
+	while (paux != NULL)
+	{
+		s << paux->toStringNodo() << endl;
+		paux = paux->getNext();
+	}
+	return s.str();
+}
+
+Contenedor_Cursos::~Contenedor_Cursos()
+{
+	cout << "Eliminando contenedor de cursos" << endl;
+
+	while (pinicio != NULL)
+	{
+		paux = pinicio;
+		pinicio = pinicio->getNext();
+		delete paux;
+	}
+}
--- Matricula/Matricula/contenedorCursos.h
+++ Matricula/Matricula/contenedorCursos.h
@@ -6,7 +6,16 @@
 
 class Contenedor_Cursos
 {
+private:
+	Nodo_Cursos *pinicio;
+	Nodo_Cursos *paux;
+public:
+	Contenedor_Cursos();
+	void insertaInicio(Curso*);
+	string toString();
 
+	
+	~Contenedor_Cursos();
 };
 #endif // !CONTENEDOR_CURSOS
 
