d623dc93c51381ae8ddc80537c53aa2ad8a4a890
Steven Cedeño <stevenced@gmail.com>
2017-05-25 21:58:41 -0600

    metodo tamano de la lista

--- Matricula/Matricula/lista.h
+++ Matricula/Matricula/lista.h
@@ -10,7 +10,7 @@ private:
 	Nodo<TipoDato>* pinicio;
 public:
 	Lista();
-	virtual void agregaInicio(TipoDato*) ;
+	virtual void agregaInicio(TipoDato*);
 	virtual int tamano();
 	virtual bool eliminaTodos();
 	~Lista();
@@ -24,7 +24,23 @@ Lista<TipoDato>::Lista() {
 }
 
 template<class TipoDato>
-void Lista<TipoDato>::agregaInicio(TipoDato* unDato) {
+inline void Lista<TipoDato>::agregaInicio(TipoDato* unDato) {
 	TipoDato* dato = new TipoDato(*unDato);
 	primerNodo = new Nodo<T>(primerNodo, *dato);
+}
+
+template<class TipoDato>
+inline int Lista<TipoDato>::tamano()
+{
+	int contador = 0;
+	while (pinicio != NULL) {
+		contador++;
+		pinicio = pinicio->getNext();
+	}
+	return contador;
+}
+
+template<class TipoDato>
+Lista<TipoDato>::~Lista() {
+	cout << "Eliminando Template..." << endl;
 }
