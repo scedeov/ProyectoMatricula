17c343788dd6cd2ac35484d21fee01b698cfe805
Steven Cedeño <stevenced@gmail.com>
2017-06-06 21:41:01 -0600

    arreglo estudiante nacional .h

--- Matricula/Matricula/estudianteNacional.h
+++ Matricula/Matricula/estudianteNacional.h
@@ -5,9 +5,9 @@
 
 class Estudiante_Nacional : public Estudiante {
 private:
+	Estudiante* estudiante;
 public:
-	Estudiante_Nacional(int);
+	Estudiante_Nacional(Estudiante*);
 	~Estudiante_Nacional();
-
 };
 #endif // !ESTUDIANTE_NACIONAL
